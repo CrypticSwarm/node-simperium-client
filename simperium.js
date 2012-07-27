b0906166771a228bf433f243061f269601243b81
CrypticSwarm <crypticswarm+git@gmail.com>
2012-07-27 18:22:29 -0500

    adds the rest of the http api
    Still need to write some tests and verify that it works.

--- simperium.js
+++ simperium.js
@@ -1,7 +1,33 @@
 var request = require('request')
 
-module.exports = { Auth: Auth,
+module.exports = {
+  Auth: Auth,
   Api: Api
+}
+
+function post(endPoint) {
+  return function apiPost(data, cb) {
+    var url = this.baseUrl + '/' + endPoint
+    request({ url: url, json: data, headers: this.headers, method: 'POST' }, cb)
+  }
+}
+
+function get(endPoint) {
+  return function apiGet(data, cb) {
+    var url = this.baseUrl + '/' + endPoint
+    var reqOpts = { url: url, headers: this.headers, method: 'GET' }
+    if (cb == null) cb = data
+    else reqOpts.qs = data
+    console.log(reqOpts)
+    request(reqOpts, cb)
+  }
+}
+
+function del(endpoint) {
+  return function apiDelete(cb) {
+    var url = this.baseUrl + '/' + endPoint
+    request({ url: url, headers: this.headers, method: 'DELETE' })
+  }
 }
 
 function Auth(appid, key, opts) {
@@ -12,17 +38,16 @@ function Auth(appid, key, opts) {
   this.host = opts.host || 'auth.simperium.com'
   this.version = opts.version || 1
   this.headers = { 'X-Simperium-API-Key':  this.key }
+  this.baseUrl = ['https:/', this.host, this.version, this.id].join('/')
 }
 
+
 Auth.prototype = {
-  create: function create(data, cb) {
-    var url = ['https:/', this.host, this.version, this.id, 'create/'].join('/')
-    request({ url: url, json: data, headers: this.headers, method: 'POST' }, cb)
-  },
-  authorize: function authorize(data, cb) {
-    var url = ['https:/', this.host, this.version, this.id, 'authorize/'].join('/')
-    request({ url: url, json: data, headers: this.headers, method: 'POST'}, cb)
-  }
+  create: post('create/'),
+  authorize: post('authorize/'),
+  update: post('update/'),
+  reset_password: post('reset_password/'),
+  delete: post('delete/')
 }
 
 function Api(appid, token, opts) {
@@ -43,12 +68,25 @@ function Bucket(name, api) {
   if (!(this instanceof Bucket)) return new Bucket(name, this)
   this.name = name
   this.api = api
+  this.baseUrl = ['https:/', api.host, api.version, api.id, this.name].join('/')
+  this.headers = api.headers
 }
 
 Bucket.prototype = {
-  list: function (cb) {
-    var api = this.api
-    var url = ['https:/', api.host, api.version, api.id, this.name, 'index'].join('/')
-    request({ url: url, headers: api.headers, method: 'GET'}, cb)
-  }
+  list: get('index'),
+  all: get('all'),
+  entry: Entry
+}
+
+function Entry(name, bucket) {
+  if (!(this instanceof Entry)) return new Entry(name, this)
+  this.baseUrl = bucket.baseUrl + '/i/' + name
+  this.headers = bucket.headers
+  this.name = name
+}
+
+Entry.prototype = {
+  get: get(''),
+  put: post(''),
+  delete: del('')
 }
